063ea9ca357175caf7b0d53da7794e8ced4f5e59
Sahya357 <sanyaverbovcki@gmail.com>
2026-03-08 15:54:04 +0200

    Add project files.

---
+++ Lab6(1.21)/Lab6(1.21)/Lab6(1.21).cpp
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Ukrainian");
+
+    int rowCount = 4, colCount = 5;
+   
+	printf("Введіть розмір масиву:\n");
+	scanf_s("%d %d", &rowCount, &colCount);
+    int a[3][4];
+    for (int i = 0; i < rowCount; i++)
+        for (int j = 0; j < colCount; j++) {
+            printf("a[%d][%d]=", i, j);
+            scanf_s("%d", &a[i][j]);
+        }
+    for (int i = 0; i < rowCount; i++) {
+        for (int j = 0; j < colCount; j++) {
+            printf("a[%d][%d]=%d\t", i, j, a[i][j]);
+        }
+        printf("\n");
+    }
+    int i1, j1, i2, j2;
+
+    printf("Введіть координати першого елемента (i1 j1): ");
+    scanf_s("%d %d", &i1, &j1);
+
+    printf("Введіть координати другого елемента (i2 j2): ");
+    scanf_s("%d %d", &i2, &j2);
+
+    int x = a[i1][j1];
+    int y = a[i2][j2];
+
+    int max;
+
+    if (x > y)
+        max = x;
+    else
+        max = y;
+
+    printf("max = %d\n", max);
+    
+}
---
+++ Lab6(2.21)/Lab6(2.21)/Lab6(2.21).cpp
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+int main() {
+    setlocale(LC_ALL, "Ukrainian");
+    const int rowCount = 4, colCount = 5, Low = -100,High = 100;
+    int a[rowCount][colCount];
+    srand(time(0));
+    for (int i = 0; i < rowCount; i++)
+        for (int j = 0; j < colCount; j++)
+            a[i][j] = Low + rand() % (High - Low + 1);
+    for (int i = 0; i < rowCount; i++) {
+        for (int j = 0; j < colCount; j++)
+            printf("a[%d][%d]=%d\t", i, j, a[i][j]);
+        printf("\n");
+    }
+    int unique[rowCount * colCount]; 
+    int k = 0;
+
+    for (int i = 0; i < rowCount; i++) {
+        for (int j = 0; j < colCount; j++) {
+            int found = 0;
+            for (int t = 0; t < k; t++) {
+                if (a[i][j] == unique[t]) {
+                    found = 1;
+                    break;
+                }
+            }
+            if (!found) {
+                unique[k] = a[i][j];
+                k++;
+            }
+        }
+    }
+
+    printf("\nКількість різних елементів матриці: %d\n", k);
+    return 0;
+
+
+}
